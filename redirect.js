4c5adffead7bc38acc43da4be4ef74ab0564e66e
JlexaII <hadajan@mail.ru>
2025-02-24 05:34:47 +0500

    Initial commit

--- redirect.js
+++ redirect.js
@@ -1,13 +1,12 @@
-const redirects = {
-    "http://sup-admission.com/": "https://jlexaii.github.io/sup-admission.com/",
-    "http://www.sup-admission.com/": "https://jlexaii.github.io/sup-admission.com/",
-    "https://sup-admission.com/": "https://jlexaii.github.io/sup-admission.com/"
-};
+const redirects = [
+    "http://sup-admission.com/",
+    "http://www.sup-admission.com/",
+    "https://sup-admission.com/"
+];
 
-// Проверяем текущий URL
 const currentURL = window.location.href;
 
-// Если URL найден в списке, делаем редирект
-if (redirects[currentURL]) {
-    window.location.replace(redirects[currentURL]); // 301 редирект
+// Если URL совпадает — редиректим
+if (redirects.includes(currentURL)) {
+    window.location.replace("https://jlexaii.github.io/sup-admission.com/");
 }

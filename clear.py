fae87b499b1dd7ab8eaadb79379ab72253fa2e91
JlexaII <hadajan@mail.ru>
2025-02-24 10:06:29 +0500

    Initial commit

--- clear.py
+++ clear.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 
 # Указываем путь к файлу
-file_path = "spa_formation-3.html"
-new_file_path = "spa_formation-3_updated.html"
+file_path = "spp_podologue-4.html"
+new_file_path = "spp_podologue-4_updated.html"
 
 # Читаем HTML-файл
 with open(file_path, "r", encoding="utf-8") as file:

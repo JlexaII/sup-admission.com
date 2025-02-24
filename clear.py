fe6e89007891853334e1dcf87305ff62b56537b8
JlexaII <hadajan@mail.ru>
2025-02-24 09:32:04 +0500

    Initial commit

--- clear.py
+++ clear.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 
 # Указываем путь к файлу
-file_path = "ssas-3.html"
-new_file_path = "ssas-3_updated.html"
+file_path = "spo_cco-4.html"
+new_file_path = "spo_cco-4_updated.html"
 
 # Читаем HTML-файл
 with open(file_path, "r", encoding="utf-8") as file:
